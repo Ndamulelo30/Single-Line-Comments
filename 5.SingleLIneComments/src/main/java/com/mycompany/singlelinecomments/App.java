fa00ce5ccaae83a240c885c334e6ef5ba4c1d23e
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 13:23:54 +0200

    Add files via upload

---
+++ 5.SingleLIneComments/src/main/java/com/mycompany/singlelinecomments/App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.singlelinecomments;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        //This is a comment
+        System.out.println("Hello World!");
+        System.out.println("How are you doing?");//This is another comment
+    }
+}
